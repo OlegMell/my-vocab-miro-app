3de1b05ab2657afb5bab035f5f753970b32174c7
Oleg Melnik <oleg.mel123@gmail.com>
2025-01-02 16:20:17 +0200

    v1

--- src/app/api/pin-word/route.ts
+++ src/app/api/pin-word/route.ts
@@ -4,6 +4,6 @@ import { WordsRequest } from '../../core/models/backend/words-request.interface'
 
 export async function PATCH( req: NextRequest ) {
     const body = await req.json();
-    const r = await wordModel.updateOne( { _id: body.wordId }, { $set: { pinned: body.pinned } } ).exec();
+    await wordModel.updateOne( { _id: body.wordId }, { $set: { pinned: body.pinned } } ).exec();
     return NextResponse.json( { data: body.wordId } as WordsRequest );
 }
