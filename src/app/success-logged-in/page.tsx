a94dcdd19da15317587ee04487765b8c3e8279dd
Oleg Melnik <oleg.mel123@gmail.com>
2025-01-25 12:44:00 +0200

    Improve login finished page

--- src/app/success-logged-in/page.tsx
+++ src/app/success-logged-in/page.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
+import logo from './../../assets/ico/logo.svg';
 
 export default async function Page() {
     return (
         <div
             style={{
                 display: 'flex',
-                width: '100vw',
-                height: '100vh',
+                height: '80vh',
                 justifyContent: 'center',
                 alignItems: 'center',
+                flexDirection: 'column',
                 whiteSpace: 'pretty'
             }}>
-            <p style={{ width: '500px', fontSize: '2rem' }}>
+            <div>
+                <Image src={logo} alt='app logo' />
+            </div>
+            <p style={{ width: 'max-content', fontSize: '2rem', padding: 0, margin: 0 }}>
                 You have been successfully logged in "My Vocab" Miro App!
                 <br />
-                <i>now you can close this tab and go back to Miro</i>
+                <br />
+                <i>now you can close this tab and go back to your Miro board</i>
             </p>
         </div>
     )
