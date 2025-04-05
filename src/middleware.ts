1534c339c9062c432afaec35414983a137ada2a4
Oleg Melnik <oleg.mel123@gmail.com>
2025-04-05 16:20:54 +0300

    Update headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,6 @@ export default function middleware() {
     response.headers.set( 'Permissions-Policy', 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()' );
     response.headers.set( 'Content-Security-Policy', `default-src 'self'; script-src-elem 'self' miro.com; frame-ancestors 'self' https://miro.com` );
     response.headers.set( 'X-Content-Type-Options', 'nosniff' );
-    response.headers.set( 'X-Frame-Options', 'DENY' );
     response.headers.set( 'X-XSS-Protection', '1; mode=block' );
 
     // Return response
