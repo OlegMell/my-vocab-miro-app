6fb40efdd8144847d2050cfea5501a5278c41e0e
Oleg Melnik <oleg.mel123@gmail.com>
2025-04-05 16:16:26 +0300

    Update headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,11 @@ export default function middleware() {
     // Set custom header
     response.headers.set( 'Cache-Control', 'no-cache, no-store' );
     response.headers.set( 'Referrer-Policy', 'strict-origin-when-cross-origin' );
-    response.headers.set( 'Permissions-Policy', 'geolocation=(), camera=(), microphone=(), fullscreen=()' );
-    // response.headers.set( 'content-security-policy', `default-src 'unsafe-inline' 'unsafe-eval' data: blob: filesystem: about: miroapp: wss: ws: *; frame-src 'unsafe-inline' 'unsafe-eval' data: blob: miroapp: *; base-uri 'unsafe-inline' about: data: *; form-action 'unsafe-inline'` );
+    response.headers.set( 'Permissions-Policy', 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()' );
+    response.headers.set( 'Content-Security-Policy', `default-src 'self'; script-src-elem 'self' miro.com; frame-ancestors 'self' https://miro.com` );
     response.headers.set( 'X-Content-Type-Options', 'nosniff' );
-    response.headers.set( 'x-xss-protection', '1; mode=block' );
+    response.headers.set( 'X-Frame-Options', 'DENY' );
+    response.headers.set( 'X-XSS-Protection', '1; mode=block' );
 
     // Return response
     return response;
