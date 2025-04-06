c1c0b7e3a01c8c53f7dc06d964b020f9ddce89a2
Oleg Melnik <oleg.mel123@gmail.com>
2025-04-06 09:23:14 +0300

    Update headers

--- src/components/SDKInit.tsx
+++ src/components/SDKInit.tsx
@@ -4,8 +4,14 @@ import { useEffect } from 'react';
 
 export const MiroSDKInit = () => {
   useEffect( () => {
+
+    const f = async () => {
+      console.log( '[canOpenPanel]: ', await miro.board.ui.canOpenPanel() )
+    }
+
+    f();
+
     miro.board.ui.on( 'icon:click', async () => {
-      // console.log( await miro.board.get() )
       await miro.board.ui.openPanel( { url: '/' } );
     } );
   } );
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,12 @@ export default function middleware() {
     const response = NextResponse.next();
 
     // Set custom header
-    // response.headers.set( 'Cache-Control', 'no-cache, no-store' );
-    // response.headers.set( 'Referrer-Policy', 'strict-origin-when-cross-origin' );
-    // response.headers.set( 'Permissions-Policy', 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()' );
-    // response.headers.set( 'Content-Security-Policy', `frame-ancestors 'self' https://miro.com` );
-    // response.headers.set( 'X-Content-Type-Options', 'nosniff' );
-    // response.headers.set( 'X-XSS-Protection', '1; mode=block' );
+    response.headers.set( 'Cache-Control', 'no-cache, no-store' );
+    response.headers.set( 'Referrer-Policy', 'strict-origin-when-cross-origin' );
+    response.headers.set( 'Permissions-Policy', 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()' );
+    response.headers.set( 'Content-Security-Policy', `frame-ancestors 'self', https://miro.com` );
+    response.headers.set( 'X-Content-Type-Options', 'nosniff' );
+    response.headers.set( 'X-XSS-Protection', '1; mode=block' );
 
     // Return response
     return response;
