7b4279d370c5982b2faeb4036fe8486be1a540bf
Oleg Melnik <oleg.mel123@gmail.com>
2025-04-08 20:47:40 +0300

    test

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,6 +1,6 @@
 import dbConnect from './db';
 
 export async function register() {
-    if (process.env.NEXT_RUNTIME !== 'nodejs') return;
+    // if (process.env.NEXT_RUNTIME !== 'nodejs') return;
     await dbConnect();
 }
