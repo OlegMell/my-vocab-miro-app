93badef6f4f01c69fa76eb483b1a728896ed15ca
Oleg Melnik <oleg.mel123@gmail.com>
2025-05-03 11:08:52 +0300

    Add withAuth wrapper for pin-word

--- src/app/api/pin-word/route.ts
+++ src/app/api/pin-word/route.ts
@@ -1,34 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import wordModel from '../../core/models/db/word.model';
 import { WordsRequest } from '../../core/models/backend/words-request.interface';
-import initMiroAPI from '../../../utils/initMiroAPI';
+import { withAuth } from '../../lib/withAuth';
 
-export async function PATCH( req: NextRequest ) {
-    const { userId: currentUserId, accessToken, miro } = initMiroAPI();
-
-    // USER VERIFICATION ------------------------------------------------------
-
-    if (!currentUserId?.trim()) {
-        return NextResponse.json({ msg: 'no user id set in cookie' }, { status: 401 });
-    } else if (!accessToken?.trim()) {
-        return NextResponse.json({ msg: 'no access token set in cookie' }, { status: 401 });
-    }
-
-    const miroApi = miro.as(currentUserId);
-
-    try {
-        const verifyAccessTokenResponse = await miroApi.tokenInfo();
-
-        if (verifyAccessTokenResponse.user.id !== currentUserId) {
-            return NextResponse.json({ msg: 'Access token did not pass the verification' }, { status: 401 });
-        }
-    } catch (err: any) {
-        return NextResponse.json(err.body, { status: err.statusCode });
-    }
-
-    // END USER VERIFICATION ------------------------------------------
-
+async function PATCH( req: any ) {
     const body = await req.json();
     await wordModel.updateOne( { _id: body.wordId }, { $set: { pinned: body.pinned } } ).exec();
     return NextResponse.json( { data: body.wordId } as WordsRequest );
 }
+
+const PATHHandler = withAuth( PATCH );
+export { PATHHandler as PATCH };
--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,35 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import usersModel from '../../core/models/db/users.model';
-import initMiroAPI from '../../../utils/initMiroAPI';
 import { User } from '../../core/models/user.inteface';
 import { withAuth } from '../../lib/withAuth';
 
 async function GET( req: any, res: any, userId?: any ) {
-
-    // const { userId: currentUserId, accessToken, miro } = initMiroAPI();
-
-    // USER VERIFICATION ------------------------------------------------------
-
-    // if ( !currentUserId?.trim() ) {
-    //     return NextResponse.json( { msg: 'no user id set in cookie' }, { status: 401 } );
-    // } else if ( !accessToken?.trim() ) {
-    //     return NextResponse.json( { msg: 'no access token set in cookie' }, { status: 401 } );
-    // }
-
-    // const miroApi = miro.as( currentUserId );
-
-    // try {
-    //     const verifyAccessTokenResponse = await miroApi.tokenInfo();
-
-    //     if ( verifyAccessTokenResponse.user.id !== currentUserId ) {
-    //         return NextResponse.json( { msg: 'Access token did not pass the verification' }, { status: 401 } );
-    //     }
-    // } catch ( err: any ) {
-    //     return NextResponse.json( err.body, { status: err.statusCode } );
-    // }
-
-    // END USER VERIFICATION ------------------------------------------
-
     const users = await usersModel.findOne( { userId } )
         .populate( 'students' )
         .exec();
@@ -40,34 +14,10 @@ async function GET( req: any, res: any, userId?: any ) {
 const GETHandler = withAuth( GET );
 export { GETHandler as GET };
 
-export async function DELETE( req: NextRequest ) {
-    const { userId: currentUserId, accessToken, miro } = initMiroAPI();
-
-    // USER VERIFICATION ------------------------------------------------------
-
-    if ( !currentUserId?.trim() ) {
-        return NextResponse.json( { msg: 'no user id set in cookie' }, { status: 401 } );
-    } else if ( !accessToken?.trim() ) {
-        return NextResponse.json( { msg: 'no access token set in cookie' }, { status: 401 } );
-    }
-
-    const miroApi = miro.as( currentUserId );
-
-    try {
-        const verifyAccessTokenResponse = await miroApi.tokenInfo();
-
-        if ( verifyAccessTokenResponse.user.id !== currentUserId ) {
-            return NextResponse.json( { msg: 'Access token did not pass the verification' }, { status: 401 } );
-        }
-    } catch ( err: any ) {
-        return NextResponse.json( err.body, { status: err.statusCode } );
-    }
-
-    // END USER VERIFICATION ------------------------------------------
-
+async function DELETE( req: any, res: any, userId?: any ) {
     const body = await req.json();
 
-    const user = await usersModel.findOne( { userId: currentUserId } ).exec();
+    const user = await usersModel.findOne( { userId } ).exec();
 
     if ( !user ) {
         return NextResponse.json( { message: 'fail', error: 'User not found' }, { status: 404 } );
@@ -78,3 +28,6 @@ export async function DELETE( req: NextRequest ) {
 
     return NextResponse.json( { data: user._id } );
 }
+
+const DELETEHandler = withAuth( DELETE );
+export { DELETEHandler as DELETE };
